cd61c005d4678ee51ee3049492e39bc43963c9b4
root <michal5bargiel@gmail.com>
2019-06-27 07:30:59 +0200

    Updates

---
+++ precache-manifest.d3c5764db24e6cde39d578c470ed94a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "248f489ffae9c7516e5d19f97e06c07f",
+    "url": "/my-site/index.html"
+  },
+  {
+    "revision": "bb9ddf9c5f6ab73a5ed7",
+    "url": "/my-site/static/css/main.03de265f.chunk.css"
+  },
+  {
+    "revision": "211f5db86b8235aff797",
+    "url": "/my-site/static/js/2.d89fe669.chunk.js"
+  },
+  {
+    "revision": "bb9ddf9c5f6ab73a5ed7",
+    "url": "/my-site/static/js/main.bab6267a.chunk.js"
+  },
+  {
+    "revision": "32bfdd9f18f755e00cad",
+    "url": "/my-site/static/js/runtime~main.0ae2f0e4.js"
+  }
+]);
